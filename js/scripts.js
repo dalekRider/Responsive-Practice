10f150e11fc0cb718da788b2f63065d75819afe8
dalekRider <jmercier76@gmail.com>
2017-10-30 12:43:51 -0400

    Almost completed
    Still needs some tweaks here and there, but getting closre

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  // burger menu toggle
+  $('.nav__toggle').click(function() {
+    $('.nav__main').toggleClass('is-open');
+    $('.hamburger').toggleClass('is-open');
+  })
+
+})
